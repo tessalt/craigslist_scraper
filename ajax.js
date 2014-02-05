66116d93b506f3ea67e0d0a1349a8b1aa9644030
Tessa Thornton <tessathornton@gmail.com>
2014-02-04 21:31:43 -0500

    minor cleanup

--- ajax.js
+++ ajax.js
@@ -1,70 +1,58 @@
-$(document).ready(function(){
+(function(){
 
- moment().format();
+   moment().format();
 
- var today = moment();
 
- var list = $("#listings");
+   var today = moment();
 
- var rootURL = 'http://toronto.en.craigslist.ca/';
+   var list = $("#listings");
 
-  function isInside(lat, lng) {
-    return geolib.isPointInside(
-      {latitude: lat, longitude: lng},
-        [
-          {latitude: 43.63484, longitude: -79.42576},
-          {latitude: 43.65410, longitude: -79.43327},
-          {latitude: 43.66214, longitude: -79.37975},
-          {latitude: 43.64565, longitude: -79.36984}
-        ]
-    );
-  }
+   var rootURL = 'http://toronto.en.craigslist.ca/';
 
-  function fetchListings(listing) {
-    $.get(listing.url, function(resp){
-      var item = jQuery.parseJSON(resp);
-      item = item[0];
-      console.log(item);
-    });
-  }
+    function isInside(lat, lng) {
+      return geolib.isPointInside(
+        {latitude: lat, longitude: lng},
+          [
+            {latitude: 43.63484, longitude: -79.42576},
+            {latitude: 43.65410, longitude: -79.43327},
+            {latitude: 43.66214, longitude: -79.37975},
+            {latitude: 43.64565, longitude: -79.36984}
+          ]
+      );
+    }
 
-  function walk(data) {
-    var listings = jQuery.parseJSON(data);
-    listings = listings[0];
-    for (var i = 0; i < listings.length; i++) {
-      var stamp = moment(listings[i].PostedDate, 'X');
-      var timeDiff = today.diff(stamp, 'days');
-      if (timeDiff < 5) {
-        if (listings[i].url !== undefined) {
-          var inBounds = isInside(listings[i].Latitude, listings[i].Longitude);
-          if (inBounds == true) {
-            $.ajax({
-              url: listings[i].url,
-              success: function(resp) {
-                walk(resp);
-              }
-            });
-          }
-        } else {
-          var inBounds = isInside(listings[i].Latitude, listings[i].Longitude);
-          console.log("inBounds: " + inBounds + "  Price: " + listings[i].Ask);
-          if (inBounds == true && listings[i].Ask > 1 && listings[i].Ask < 1101) {
-            var li = $("<li>");
-            var html = "<a href='" + rootURL + listings[i].PostingURL + "'>" + listings[i].PostingTitle + "</a>";
+    function walk(data) {
+      var listings = jQuery.parseJSON(data);
+      listings = listings[0];
+      for (var i = 0; i < listings.length; i++) {
+        var stamp = moment(listings[i].PostedDate, 'X');
+        var timeDiff = today.diff(stamp, 'days');
+        if (timeDiff < 5) {
+          if (listings[i].url !== undefined) {
+            var inBounds = isInside(listings[i].Latitude, listings[i].Longitude);
             if (inBounds == true) {
-              html += "<strong>INBOUNDS</strong>";
+              $.ajax({
+                url: listings[i].url,
+                success: function(resp) {
+                  walk(resp);
+                }
+              });
             }
-            li.html(html);
-            list.append(li);
+          } else {
+            var inBounds = isInside(listings[i].Latitude, listings[i].Longitude);
+            if (inBounds == true && listings[i].Ask > 1 && listings[i].Ask < 1101) {
+              var li = $("<li>");
+              var html = "<a href='" + rootURL + listings[i].PostingURL + "'>" + listings[i].PostingTitle + "</a>";
+              li.html(html);
+              list.append(li);
+            }
           }
         }
       }
     }
-  }
 
+    $.get('/listings', function(data){
+      walk(data);
+    });
 
-  $.get('/listings', function(data){
-    walk(data);
-  });
-
-});
+})();
